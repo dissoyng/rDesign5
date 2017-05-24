219e5b788a3832693299a27b5680db81a7d7c537
Disso <jrD1sso@gmail.com>
2017-05-25 00:16:40 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*global $, jQuery, alert*/
+$(function () {
+    'use strict';
+    //Nice Scroll
+    $("html").niceScroll({
+        cursorwidth: "10px",
+        cursorborder: "none",
+        cursorcolor: "#e8115b"
+    });
+    
+});
